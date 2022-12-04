46ce024e199823f320a67bfcfbfbde7948e954d1
sha1-1 <84086478+sha1-1@users.noreply.github.com>
2022-12-04 17:45:58 +0800

    Add files via upload

---
+++ st_DATA/st_run.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# pip install pandas -i https://mirrors.aliyun.com/pypi/simple
+# pip install -U requests[security]
+""" my_wx: windforyou_ """
+import os
+os.system(r'streamlit run 主页.py')
